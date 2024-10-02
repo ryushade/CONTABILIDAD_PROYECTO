54f93ea73b9969bb319c573950ea6d39e82ef992
ryushade <77291154@usat.pe>
2024-10-02 09:13:40 -0500

    Refactor CSS styles in index.html and styles.css

--- app/controllers/contable/routes.py
+++ app/controllers/contable/routes.py
@@ -1,5 +1,5 @@
 from flask import request, redirect, url_for, flash, session, jsonify, render_template
-from app.models.contable_models import Usuario
+from app.models.contable_models import Usuario, Cuenta
 from . import accounting_bp
 from app import db
 
@@ -32,7 +32,12 @@ def logout():
     flash('Logout exitoso', 'success')
     return redirect(url_for('contable.login'))
 
-@accounting_bp.route('/cuentas')
+@accounting_bp.route('/cuentas', methods=['GET'])
 def cuentas():
-    return render_template('contable/cuentas/cuentas.html')
+    page = request.args.get('page', 1, type=int)
+    per_page = request.args.get('per_page', 50, type=int)
+
+    
+    cuentas = Cuenta.query.paginate(page=page, per_page=per_page)
+    return render_template('contable/cuentas/cuentas.html', cuentas=cuentas.items, page=page, total_pages = cuentas.pages, per_page=per_page, max=max, min=min) 
 
